c6e247e3125140d8c84884be33ece9a6675df173
imtiaz <programmer.imtiaz@gmail.com>
2022-10-06 15:58:57 +0600

    feat: user profile UI implemented

---
+++ app/src/main/java/com/imtiaz/githubuserstest/presentation/profile/ProfileFragment.kt
@@ -0,0 +1,26 @@
+package com.imtiaz.githubuserstest.presentation.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.imtiaz.githubuserstest.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var _binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(layoutInflater)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
