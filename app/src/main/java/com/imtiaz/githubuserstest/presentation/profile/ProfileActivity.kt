371f54ed44024164f7e83abf9652cbc17bf16751
imtiaz <programmer.imtiaz@gmail.com>
2022-10-13 13:24:21 +0600

    feat: Profile Compose activity created and initial toolbar setup done.

--- app/src/main/java/com/imtiaz/githubuserstest/presentation/profile/ProfileActivity.kt
+++ app/src/main/java/com/imtiaz/githubuserstest/presentation/profile/ProfileActivity.kt
@@ -1,28 +1,49 @@
 package com.imtiaz.githubuserstest.presentation.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.imtiaz.githubuserstest.core.extensions.CURRENT_USER
+import com.imtiaz.githubuserstest.data.local.db.entity.GithubUser
 import com.imtiaz.githubuserstest.presentation.profile.ui.theme.GithubUsersTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GithubUsersTheme {
                 // A surface container using the 'background' color from the theme
+                val user = getUsersFromIntent(intent)
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        topBar = {
+                            TopBar(user?.login ?: "Github User App", this)
+                        }
+                    ) {
+
+                    }
                 }
             }
         }
@@ -30,14 +51,30 @@ class ProfileActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TopBar(userName: String, activity: Activity) {
+    TopAppBar(
+        title = { Text(text = userName) },
+        navigationIcon = {
+            IconButton(
+                onClick = { activity.finish() }
+            ) {
+                Icon(Icons.Filled.ArrowBack, contentDescription = "Back Icon")
+            }
+        }
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    GithubUsersTheme {
-        Greeting("Android")
+    GithubUsersTheme {}
+}
+
+fun getUsersFromIntent(intent: Intent): GithubUser? {
+    return if (Build.VERSION.SDK_INT >= 33) {
+        intent.getParcelableExtra<GithubUser>(CURRENT_USER, GithubUser::class.java)
+    } else {
+        intent.getParcelableExtra<GithubUser>(CURRENT_USER)
     }
 }
+
