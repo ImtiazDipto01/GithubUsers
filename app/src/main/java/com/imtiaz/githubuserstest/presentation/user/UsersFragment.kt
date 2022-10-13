dc94f8894e3beba8d19fe2f1de3ec065c8d7de5a
imtiaz <programmer.imtiaz@gmail.com>
2022-10-13 11:26:21 +0600

    feat: new compose activity added and some method commenting added

--- app/src/main/java/com/imtiaz/githubuserstest/presentation/user/UsersFragment.kt
+++ app/src/main/java/com/imtiaz/githubuserstest/presentation/user/UsersFragment.kt
@@ -1,13 +1,11 @@
 package com.imtiaz.githubuserstest.presentation.user
 
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -20,14 +18,15 @@ import com.imtiaz.githubuserstest.core.extensions.*
 import com.imtiaz.githubuserstest.data.local.db.entity.GithubUser
 import com.imtiaz.githubuserstest.databinding.FragmentUsersBinding
 import com.imtiaz.githubuserstest.presentation.main.MainViewModel
+import com.imtiaz.githubuserstest.presentation.profile.ProfileActivity
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.launch
 import java.io.IOException
+
 
 @AndroidEntryPoint
 class UsersFragment : Fragment() {
@@ -99,8 +98,12 @@ class UsersFragment : Fragment() {
      * @param text - contains search text
      */
     private fun delayAndStartSearch(text: String) {
+        // canceling previous job so that new started coroutine job
+        // don't get overlap with previous job
         searchJob?.cancel()
+
         if(text.length > 1){
+            // if search text contains at least 2 characters then we'll start processing the search
             searchJob = lifecycleScope.launch(Dispatchers.Main) {
                 delay(200)
 
@@ -109,7 +112,8 @@ class UsersFragment : Fragment() {
             }
         }
         if(text.isEmpty()) {
-            Log.e("isEmptyText", "True")
+            // if search text is empty we're clearing the full list
+            // showing previous full users list
             userAdapter.apply {
                 clearList()
                 submitList(users)
@@ -117,6 +121,11 @@ class UsersFragment : Fragment() {
         }
     }
 
+    /**
+     * here we're collecting the full cached user list from db
+     * and updating our UI. this is our only data source for showing users in recyclerview
+     *
+     */
     private fun collectUsersFromDb() {
         lifecycleScope.launchWhenStarted {
             viewModel.getUsers().collect {
@@ -130,6 +139,11 @@ class UsersFragment : Fragment() {
         }
     }
 
+    /**
+     * This function only observe the loading and error state
+     * when we're fetching user list from API
+     *
+     */
     private fun observeFetchUsers() {
         lifecycleScope.launchWhenStarted {
             viewModel.fetchUsersFlow.collect {
@@ -229,6 +243,11 @@ class UsersFragment : Fragment() {
         }
     }
 
+    /**
+     * This function will start show & hide the skeletons only for initial loading.
+     *
+     * @param isLoading -> return [true] if intial loading started otherwise [false]
+     */
     private fun handleInitialLoading(isLoading: Boolean) = _binding.apply {
         layoutSkeleton.parentLayout.isVisible = isLoading
         if(isLoading)
@@ -236,14 +255,39 @@ class UsersFragment : Fragment() {
         else layoutSkeleton.shimmerViewContainer.stopShimmer()
     }
 
+    /**
+     * If there is no any data in user adapter that means,
+     * we need to fetch the first page data.
+     *
+     */
     private fun checkIfNeedInitialFetch() {
         if (userAdapter.itemCount == 0)
             viewModel.fetchUsers(FIRST_PAGE)
     }
 
+    /**
+     * when App user click any Github users from user list
+     * this lambda function will invoke and will start profile activity
+     *
+     * @return Unit
+     */
     private fun onItemClick(): (GithubUser) -> Unit = {
-        requireActivity().navigateTo(usersToProfile(it))
+        //requireActivity().navigateTo(usersToProfile(it))
+
+        // here we're starting Profile Activity, because
+        // this is only activity written in compose, otherwise will launch a fragment
+        Intent(requireActivity(), ProfileActivity::class.java).apply {
+            putExtra(CURRENT_USER, it)
+            startActivity(this)
+        }
     }
 
+    /**
+     * on user list scrolling, In parallel we'll fetch updated user info
+     * this lambda function will invoke when a new page(SinceId) will found that
+     * we need to fetch updated user info for that page.
+     *
+     * @return
+     */
     private fun onScrollUpdateData(): (Int) -> Unit = { viewModel.updateUsersData(it) }
 }
